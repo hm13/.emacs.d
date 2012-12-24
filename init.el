b7e5d75019ddc2311e7374b192d4150a3e3822c4
U-13-PC\13 <13@13-PC.(none)>
2012-12-24 17:02:00 +0900

    set default compile command in c-mode

--- init.el
+++ init.el
@@ -130,29 +130,40 @@
 
 ;;;Ruby自動改行とインデント設定
 (add-hook 'ruby-mode-hook
-		  '(lambda()
-			 (define-key ruby-mode-base-map "\C-m" 'newline-and-indent)
-			 (set-default-coding-systems 'utf-8)
-;			 (setq ruby-deep-indent-paren-style t)
-			 ))
+	  '(lambda()
+	     (define-key ruby-mode-base-map "\C-m" 'newline-and-indent)
+	     (set-default-coding-systems 'utf-8)
+	     ;; (setq ruby-deep-indent-paren-style t)
+	     ))
 
 ;;;c++自動改行とインデント設定
 (add-hook 'c++-mode-hook
           '(lambda ()
              (c-set-style "linux")	;gnu, k&r, bsd, stroustrup, whitesmith, ellemtel, linux, cc-mode, python etc...
-			 (setq c-basic-offset 3) ;インデント幅
-			 (setq tab-width c-basic-offset)
-			 (setq indent-tabs-mode t) ;インデントはタブで
-			 ))
+	     (setq c-basic-offset 3) ;インデント幅
+	     (setq tab-width c-basic-offset)
+	     (setq indent-tabs-mode t) ;インデントはタブで
+	     ))
+
 
 ;;;c自動改行+インデント
 (add-hook 'c-mode-common-hook
-		  '(lambda ()
-		     (c-set-style "linux")
-		; センテンスの終了である ';' を入力したら、自動改行+インデント
-		     (setq c-basic-offset 4) ;インデント幅
-		     (setq tab-width c-basic-offset)
-		     (setq indent-tabs-mode t) ;インデントはタブで
-		; RET キーで自動改行+インデント
-		     (define-key c-mode-base-map "\C-m" 'newline-and-indent)
-		     ))
+	  '(lambda ()
+	     (c-set-style "linux")
+	     ;; センテンスの終了である ';' を入力したら、自動改行+インデント
+	     (setq c-basic-offset 4) ;インデント幅
+	     (setq tab-width c-basic-offset)
+	     (setq indent-tabs-mode t) ;インデントはタブで
+	     ;; RET キーで自動改行+インデント
+	     (define-key c-mode-base-map "\C-m" 'newline-and-indent)
+	     (unless (file-exists-p "Makefile")
+	       (set (make-local-variable 'compile-command)
+		    ;; $(CC) -c -o $@ $(CPPFLAGS) $(CFLAGS) $<
+		    (let ((file (file-name-nondirectory buffer-file-name)))
+                      (format "%s -o %s.out %s %s %s"; (format "%s -c -o %s.o %s %s %s" ;;default
+		      	      (or (getenv "CC") "gcc") 
+		      	      (file-name-sans-extension file)
+		      	      (or (getenv "CPPFLAGS") "-DDEBUG=9")
+		      	      (or (getenv "CFLAGS") "-ansi -pedantic -Wall -g")
+			      file))))
+	     ))
